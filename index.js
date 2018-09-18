fa6adca2e47747a6b5d63763cf435aa8d25f8729
nicoal tettey <nicoaltettey@roberts-MacBook-Air.local>
2018-09-18 10:49:15 +0100

    Get weather description

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ fetch(
     return response.json();
   })
   .then(function(body) {
-    console.log(body);
+    getWeatherDescription(body);
   })
   .catch(function(error) {
     console.log(error);
   });
+
+// fetch(
+//   "https://api.unsplash.com/search/photos?client_id=96c098bfd4adba421192e5778de230c7867e9c4dfeaf3836100bfc69ebc18553&page=3&query=office"
+// )
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(body) {
+//     console.log(body);
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+
+const getWeatherDescription = data => {
+  console.log(data.weather[0].description);
+};
