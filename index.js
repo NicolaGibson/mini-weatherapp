03f7fa4afb683efec5636ab03d8d4dd51a8d4c8a
nicoal tettey <nicoaltettey@roberts-MacBook-Air.local>
2018-09-18 11:07:10 +0100

    Get photo based on weather description

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
+const params = { description: "" };
+
 fetch(
-  "http://api.openweathermap.org/data/2.5/weather?APPID=7544db96b0f129d2f80d356fb7c5de00&q=London"
+  "http://api.openweathermap.org/data/2.5/weather?APPID=7544db96b0f129d2f80d356fb7c5de00&q=Dubai"
 )
   .then(function(response) {
     return response.json();
   })
   .then(function(body) {
     getWeatherDescription(body);
+    getWeatherPhotos(params.description);
   })
   .catch(function(error) {
     console.log(error);
   });
 
-// fetch(
-//   "https://api.unsplash.com/search/photos?client_id=96c098bfd4adba421192e5778de230c7867e9c4dfeaf3836100bfc69ebc18553&page=3&query=office"
-// )
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(body) {
-//     console.log(body);
-//   })
-//   .catch(function(error) {
-//     console.log(error);
-//   });
+const getWeatherPhotos = description => {
+  fetch(
+    `https://api.unsplash.com/search/photos?client_id=96c098bfd4adba421192e5778de230c7867e9c4dfeaf3836100bfc69ebc18553&page=3&query=${description}`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(body) {
+      console.log(body);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
 
 const getWeatherDescription = data => {
-  console.log(data.weather[0].description);
+  params.description = data.weather[0].description;
+  console.log(params.description);
 };
